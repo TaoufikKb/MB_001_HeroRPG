dc1da384bfe6ac790d30548f283474cfbebcbb00
TaoufikKb <taoufik.kibba@gmail.com>
2024-08-08 19:07:38 +0200

    Phase 1 done !

---
+++ Assets/Scripts/EnemiesSpawner.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemiesSpawner : MonoBehaviour
+{
+    [SerializeField] Enemy _enemyPrefab;
+
+    [Header("Settings")]
+    [SerializeField] int _maxEnemiesCount;
+    [SerializeField] float _timeBetweenSpawns;
+    [SerializeField] float _spawnRadius;
+
+    int _count;
+    float _spawnTime;
+
+    void Start()
+    {
+        _count = 0;
+        _spawnTime = Time.time;
+    }
+
+
+    void Update()
+    {
+        if (_count <_maxEnemiesCount && Time.time > _spawnTime+_timeBetweenSpawns)
+        {
+            _spawnTime = Time.time;
+
+            SpawnEnemy();
+            _count++;
+        }
+    }
+
+    void SpawnEnemy()
+    {
+        Vector3 rand = Random.onUnitSphere;
+        rand.y = 0;
+        rand.Normalize();
+
+        Vector3 position = Player.instance.transform.position + rand * _spawnRadius;
+        Instantiate(_enemyPrefab, position, Quaternion.identity, transform);
+    }
+}
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,12 +1,14 @@
-using System;
+
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    public static Player instance;
+
     [SerializeField] Animator _animator;
-    [SerializeField] Weapon _weapon;
+    [SerializeField] Weapon[] _weapons;
 
 
     //[Header("Fight Settings")]
@@ -16,24 +18,39 @@ public class Player : MonoBehaviour
     BehaviourIdle _behaviourIdle;
     BehaviourStrike _behaviourStrike;
 
+    Weapon _weapon;
 
+
+    void Awake()
+    {
+        instance = this;
+    }
     void Start()
     {
         _behaviourRun = _animator.GetBehaviour<BehaviourRun>();
         _behaviourIdle = _animator.GetBehaviour<BehaviourIdle>();
         _behaviourStrike = _animator.GetBehaviour<BehaviourStrike>();
 
-        EquipWeapon();
+        EquipWeapon(_weapons[Random.Range(0, _weapons.Length)]);
         InitEvents();
     }
     
-    void EquipWeapon()
+    void EquipWeapon(Weapon weapon)
     {
+        foreach (var item in _weapons)
+        {
+            item.gameObject.SetActive(false);
+        }
+        weapon.gameObject.SetActive(true);
+
+        _weapon = weapon;
+
         _behaviourRun.maxSpeed = _weapon.movementSpeed;
         _behaviourIdle.enemyDetectionRadius = _weapon.range;
         _behaviourStrike.weapon = _weapon;
 
         _animator.SetFloat("AttackSpeed",_weapon.attackSpeed);
+        _animator.SetFloat("MovementSpeed", _weapon.movementSpeed);
     }
 
     void InitEvents()
